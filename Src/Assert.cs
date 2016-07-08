49764ba6c2810f3ed0c5f9fa03073a247a097422
J Wyman <jeremy.wyman@outlook.com>
2016-07-08 00:42:23 -0400

    Update Assert and Ensure to make more sense and be more flexible.

--- Src/Assert.cs
+++ Src/Assert.cs
@@ -2,168 +2,258 @@
 using System;
 using System.Diagnostics;
 using System.IO;
+using System.Runtime.CompilerServices;
 
 namespace XTypes
 {
     internal static class Assert
     {
         [Conditional("DEBUG")]
+        [DebuggerHidden]
+        [DebuggerStepThrough]
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static void AreEqual<T>(T value, T expected)
         {
-            InternalAssert(value.Equals(expected));
+            NotNull(expected);
+            NotNull(value);
+
+            if (!value.Equals(expected))
+                Fail($"Expected '{expected}', found '{value}'.");
         }
 
         [Conditional("DEBUG")]
+        [DebuggerHidden]
+        [DebuggerStepThrough]
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static void DirectoryExists(string path)
         {
             ValidString(path);
-            InternalAssert(Directory.Exists(path));
+
+            if (!Directory.Exists(path))
+                Fail($"'{path}' does not exist or is inaccessible.");
         }
 
         [Conditional("DEBUG")]
+        [DebuggerHidden]
+        [DebuggerStepThrough]
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static void EnumDefined<T>(T value)
         {
             NotNull(value);
-            InternalAssert(typeof(T).IsEnum && Enum.IsDefined(typeof(T), value));
+
+            if (!(typeof(T).IsEnum && Enum.IsDefined(typeof(T), value)))
+                Fail($"'{value}' is not a member of '{typeof(T)}'.");
         }
 
         [Conditional("DEBUG")]
+        [DebuggerHidden]
+        [DebuggerStepThrough]
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static void Fail(string message)
+        {
+            throw new AssertionException(message);
+        }
+
+        [Conditional("DEBUG")]
+        [DebuggerHidden]
+        [DebuggerStepThrough]
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static void FileExist(string path)
         {
             ValidString(path);
-            InternalAssert(File.Exists(path));
+
+            if (!File.Exists(path))
+                Fail($"'{path}' does not exist or is inaccessible.");
         }
 
         [Conditional("DEBUG")]
+        [DebuggerHidden]
+        [DebuggerStepThrough]
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static void Greater<T>(T value, T lesser)
-            where T : IComparable, IComparable<T>
+            where T : IComparable<T>
         {
-            NotNull(value);
             NotNull(lesser);
-            InternalAssert(value.CompareTo(lesser) > 0);
+            NotNull(value);
+
+            if (value.CompareTo(lesser) <= 0)
+                Fail($"Expected greater than '{lesser}', found '{value}'.");
         }
 
         [Conditional("DEBUG")]
+        [DebuggerHidden]
+        [DebuggerStepThrough]
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static void GreaterOrEqual<T>(T value, T lesser)
-            where T : IComparable, IComparable<T>
+            where T : IComparable<T>
         {
             NotNull(value);
-            NotNull(lesser);
-            InternalAssert(value.CompareTo(lesser) >= 0);
+
+            if (value.CompareTo(lesser) < 0)
+                Fail($"Expected greater than or equal to '{lesser}', found '{value}'.");
         }
 
         [Conditional("DEBUG")]
+        [DebuggerHidden]
+        [DebuggerStepThrough]
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static void IsFalse(bool value)
         {
-            InternalAssert(!value);
+            if (value)
+                Fail("Expected false, found true.");
         }
 
         [Conditional("DEBUG")]
+        [DebuggerHidden]
+        [DebuggerStepThrough]
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static void IsTrue(bool value)
         {
-            InternalAssert(value);
+            if (!value)
+                Fail("Expected true, found false.");
         }
 
         [Conditional("DEBUG")]
+        [DebuggerHidden]
+        [DebuggerStepThrough]
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static void IsType<TType>(object value)
         {
-            NotNull(value);
-
-            InternalAssert(value.GetType() == typeof(TType));
+            if (value.GetType() != typeof(TType))
+                Fail($"Unexpected type encountered. Expected '{typeof(TType)}', found '{value.GetType()}'.");
         }
 
         [Conditional("DEBUG")]
+        [DebuggerHidden]
+        [DebuggerStepThrough]
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static void Less<T>(T value, T greater)
-            where T : IComparable, IComparable<T>
+            where T : IComparable<T>
         {
             NotNull(value);
-            NotNull(greater);
-            InternalAssert(value.CompareTo(greater) < 0);
+
+            if (value.CompareTo(greater) >= 0)
+                Fail($"Invalid argument encountered. Expected less than '{greater}', found '{value}'.");
         }
 
         [Conditional("DEBUG")]
+        [DebuggerHidden]
+        [DebuggerStepThrough]
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static void LessOrEqual<T>(T value, T greater)
-            where T : IComparable, IComparable<T>
+            where T : IComparable<T>
         {
-            NotNull(value);
             NotNull(greater);
-            InternalAssert(value.CompareTo(greater) <= 0);
+            NotNull(value);
+
+            if (value.CompareTo(greater) > 0)
+                Fail($"Invalid argument encountered. Expected less then or equal to '{greater}', found '{value}'.");
         }
 
         [Conditional("DEBUG")]
+        [DebuggerHidden]
+        [DebuggerStepThrough]
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static void NotEqual<T>(T value, T expected)
         {
-            InternalAssert(!value.Equals(expected));
+            NotNull(expected);
+            NotNull(value);
+
+            if (value.Equals(expected))
+                Fail($"Invalid argument encountered. Expected {value}, found '{expected}'.");
         }
 
         [Conditional("DEBUG")]
+        [DebuggerHidden]
+        [DebuggerStepThrough]
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static void NotNull(object value)
         {
-            InternalAssert(value != null);
+            if (value == null)
+                Fail("Unexpected null value encountered.");
         }
 
         [Conditional("DEBUG")]
+        [DebuggerHidden]
+        [DebuggerStepThrough]
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static void ValidArrayIndex(int value, Array array)
         {
             NotNull(array);
-            InternalAssert(value >= 0 && value < array.Length);
+
+            if (value < 0 || value >= array.Length)
+                Fail($"Expected greater than or equal to 0 and less than {array.Length}, found {value}.");
         }
 
         [Conditional("DEBUG")]
+        [DebuggerHidden]
+        [DebuggerStepThrough]
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static void ValidArrayIndex(long value, Array array)
         {
             NotNull(array);
-            InternalAssert(value >= 0 && value < array.LongLength);
+
+            if (value < 0 || value >= array.LongLength)
+                Fail($"Expected greater than or equal to 0 and less than {array.LongLength}, found {value}.");
         }
 
         [Conditional("DEBUG")]
-        public static void ValidArrayIndex(int value, string array)
+        [DebuggerHidden]
+        [DebuggerStepThrough]
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static void ValidArrayIndex(int value, string array, string name)
         {
             NotNull(array);
-            InternalAssert(value >= 0 && value < array.Length);
+
+            if (value < 0 || value >= array.Length)
+                Fail($"Expected greater than or equal to 0 and less than {array.Length}, found {value}.");
         }
 
         [Conditional("DEBUG")]
+        [DebuggerHidden]
+        [DebuggerStepThrough]
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static void ValidString(string value)
         {
-            InternalAssert(!String.IsNullOrWhiteSpace(value));
+            if (String.IsNullOrWhiteSpace(value))
+                Fail("Unexpected null or empty value encountered.");
         }
 
         [Conditional("DEBUG")]
+        [DebuggerHidden]
+        [DebuggerStepThrough]
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static void ValidUriString(string value)
         {
             ValidString(value);
 
-            InternalAssert(Uri.IsWellFormedUriString(value, UriKind.RelativeOrAbsolute));
+            if (!Uri.IsWellFormedUriString(value, UriKind.RelativeOrAbsolute))
+                Fail("Unexpected malformed URI value encountered.");
         }
 
         [Conditional("DEBUG")]
+        [DebuggerHidden]
+        [DebuggerStepThrough]
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static void WithinRange<T>(T value, T lowerLimit, T upperLimit)
             where T : IComparable, IComparable<T>
         {
             Greater(value, lowerLimit);
             Less(value, upperLimit);
         }
-
-        [Conditional("DEBUG")]
-        private static void InternalAssert(bool condition)
-        {
-            if (!condition)
-            {
-                if (Debugger.IsAttached)
-                {
-                    Debugger.Break();
-                }
-                else
-                {
-                    throw new AssertionException("Internal assertion failed.");
-                }
-            }
-        }
     }
 
     internal class AssertionException : Exception
     {
-        public AssertionException(string message) : base(message) { }
+        public AssertionException(string message)
+            : base(message)
+        { }
+
+        public AssertionException(string message, Exception innerException)
+            : base(message, innerException)
+        { }
+
+        internal AssertionException(System.Runtime.Serialization.SerializationInfo info, System.Runtime.Serialization.StreamingContext context)
+        { }
     }
 }
--- Src/Ensure.cs
+++ Src/Ensure.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.ComponentModel;
+using System.Diagnostics;
 using System.IO;
 using System.Runtime.CompilerServices;
 
@@ -7,6 +8,20 @@ namespace XTypes
 {
     internal static class Ensure
     {
+        [DebuggerHidden]
+        [DebuggerStepThrough]
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static void AreEqual<T>(T value, T expected)
+        {
+            Assert.NotNull(expected);
+            NotNull(value);
+
+            if (!value.Equals(expected))
+                throw new EnsureException($"Invalid argument encountered. Expected '{expected}', found '{value}'.");
+        }
+
+        [DebuggerHidden]
+        [DebuggerStepThrough]
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static void AreEqual<T>(T value, T expected, string name)
         {
@@ -18,6 +33,19 @@ namespace XTypes
                 throw new ArgumentException($"Invalid argument encountered. Expected '{expected}', found '{value}'.", name);
         }
 
+        [DebuggerHidden]
+        [DebuggerStepThrough]
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static void DirectoryExists(string path)
+        {
+            ValidString(path);
+
+            if (!Directory.Exists(path))
+                throw new EnsureException("Invalid argument encountered.", new DirectoryNotFoundException($"'{path}' does not exist or is inaccessible."));
+        }
+
+        [DebuggerHidden]
+        [DebuggerStepThrough]
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static void DirectoryExists(string path, string name)
         {
@@ -28,6 +56,19 @@ namespace XTypes
                 throw new ArgumentException("Invalid argument encountered.", name, new DirectoryNotFoundException($"'{path}' does not exist or is inaccessible."));
         }
 
+        [DebuggerHidden]
+        [DebuggerStepThrough]
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static void EnumDefined<T>(T value)
+        {
+            NotNull(value);
+
+            if (!(typeof(T).IsEnum && Enum.IsDefined(typeof(T), value)))
+                throw new EnsureException("Invalid argument encountered.", new InvalidEnumArgumentException($"'{value}' is not a member of '{typeof(T)}'."));
+        }
+
+        [DebuggerHidden]
+        [DebuggerStepThrough]
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static void EnumDefined<T>(T value, string name)
         {
@@ -38,6 +79,19 @@ namespace XTypes
                 throw new ArgumentException("Invalid argument encountered.", name, new InvalidEnumArgumentException($"'{value}' is not a member of '{typeof(T)}'."));
         }
 
+        [DebuggerHidden]
+        [DebuggerStepThrough]
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static void FileExist(string path)
+        {
+            ValidString(path);
+
+            if (!File.Exists(path))
+                throw new EnsureException("Invalid argument encountered.", new FileNotFoundException($"'{path}' does not exist or is inaccessible."));
+        }
+
+        [DebuggerHidden]
+        [DebuggerStepThrough]
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static void FileExist(string path, string name)
         {
@@ -48,6 +102,21 @@ namespace XTypes
                 throw new ArgumentException("Invalid argument encountered.", name, new FileNotFoundException($"'{path}' does not exist or is inaccessible."));
         }
 
+        [DebuggerHidden]
+        [DebuggerStepThrough]
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static void Greater<T>(T value, T lesser)
+            where T : IComparable<T>
+        {
+            Assert.NotNull(lesser);
+            NotNull(value);
+
+            if (value.CompareTo(lesser) <= 0)
+                throw new EnsureException($"Invalid argument encountered. Expected greater than '{lesser}', found '{value}'.");
+        }
+
+        [DebuggerHidden]
+        [DebuggerStepThrough]
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static void Greater<T>(T value, T lesser, string name)
             where T : IComparable<T>
@@ -60,6 +129,21 @@ namespace XTypes
                 throw new ArgumentOutOfRangeException(name, $"Invalid argument encountered. Expected greater than '{lesser}', found '{value}'.");
         }
 
+        [DebuggerHidden]
+        [DebuggerStepThrough]
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static void GreaterOrEqual<T>(T value, T lesser)
+            where T : IComparable<T>
+        {
+            Assert.NotNull(lesser);
+            NotNull(value);
+
+            if (value.CompareTo(lesser) < 0)
+                throw new EnsureException($"Invalid argument encountered. Expected greater than or equal to '{lesser}', found '{value}'.");
+        }
+
+        [DebuggerHidden]
+        [DebuggerStepThrough]
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static void GreaterOrEqual<T>(T value, T lesser, string name)
             where T : IComparable<T>
@@ -72,6 +156,17 @@ namespace XTypes
                 throw new ArgumentOutOfRangeException(name, $"Invalid argument encountered. Expected greater than or equal to '{lesser}', found '{value}'.");
         }
 
+        [DebuggerHidden]
+        [DebuggerStepThrough]
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static void IsFalse(bool value)
+        {
+            if (value)
+                throw new EnsureException("Invalid argument encountered. Expected false, found true.");
+        }
+
+        [DebuggerHidden]
+        [DebuggerStepThrough]
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static void IsFalse(bool value, string name)
         {
@@ -79,6 +174,17 @@ namespace XTypes
                 throw new ArgumentException("Invalid argument encountered. Expected false, found true.", name);
         }
 
+        [DebuggerHidden]
+        [DebuggerStepThrough]
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static void IsTrue(bool value)
+        {
+            if (!value)
+                throw new EnsureException("Invalid argument encountered. Expected true, found false.");
+        }
+
+        [DebuggerHidden]
+        [DebuggerStepThrough]
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static void IsTrue(bool value, string name)
         {
@@ -86,6 +192,17 @@ namespace XTypes
                 throw new ArgumentException("Invalid argument encountered. Expected true, found false.", name);
         }
 
+        [DebuggerHidden]
+        [DebuggerStepThrough]
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static void IsType<TType>(object value)
+        {
+            if (value.GetType() != typeof(TType))
+                throw new EnsureException($"Unexpected type encountered. Expected '{typeof(TType)}', found '{value.GetType()}'.");
+        }
+
+        [DebuggerHidden]
+        [DebuggerStepThrough]
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static void IsType<TType>(object value, string name)
         {
@@ -95,6 +212,21 @@ namespace XTypes
                 throw new ArgumentException($"Unexpected type encountered. Expected '{typeof(TType)}', found '{value.GetType()}'.", name);
         }
 
+        [DebuggerHidden]
+        [DebuggerStepThrough]
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static void Less<T>(T value, T greater)
+            where T : IComparable<T>
+        {
+            Assert.NotNull(greater);
+            NotNull(value);
+
+            if (value.CompareTo(greater) >= 0)
+                throw new EnsureException($"Invalid argument encountered. Expected less than '{greater}', found '{value}'.");
+        }
+
+        [DebuggerHidden]
+        [DebuggerStepThrough]
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static void Less<T>(T value, T greater, string name)
             where T : IComparable<T>
@@ -107,6 +239,21 @@ namespace XTypes
                 throw new ArgumentOutOfRangeException(name, $"Invalid argument encountered. Expected less than '{greater}', found '{value}'.");
         }
 
+        [DebuggerHidden]
+        [DebuggerStepThrough]
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static void LessOrEqual<T>(T value, T greater)
+            where T : IComparable<T>
+        {
+            Assert.NotNull(greater);
+            NotNull(value);
+
+            if (value.CompareTo(greater) > 0)
+                throw new EnsureException($"Invalid argument encountered. Expected less then or equal to '{greater}', found '{value}'.");
+        }
+
+        [DebuggerHidden]
+        [DebuggerStepThrough]
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static void LessOrEqual<T>(T value, T greater, string name)
             where T : IComparable<T>
@@ -119,6 +266,20 @@ namespace XTypes
                 throw new ArgumentOutOfRangeException(name, $"Invalid argument encountered. Expected less then or equal to '{greater}', found '{value}'.");
         }
 
+        [DebuggerHidden]
+        [DebuggerStepThrough]
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static void NotEqual<T>(T value, T expected)
+        {
+            Assert.NotNull(expected);
+            NotNull(value);
+
+            if (value.Equals(expected))
+                throw new EnsureException($"Invalid argument encountered. Expected {value}, found '{expected}'.");
+        }
+
+        [DebuggerHidden]
+        [DebuggerStepThrough]
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static void NotEqual<T>(T value, T expected, string name)
         {
@@ -130,6 +291,17 @@ namespace XTypes
                 throw new ArgumentException($"Invalid argument encountered. Expected {value}, found '{expected}'.");
         }
 
+        [DebuggerHidden]
+        [DebuggerStepThrough]
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static void NotNull(object value)
+        {
+            if (value == null)
+                throw new EnsureException("Unexpected null value encountered.");
+        }
+
+        [DebuggerHidden]
+        [DebuggerStepThrough]
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static void NotNull(object value, string name)
         {
@@ -139,16 +311,19 @@ namespace XTypes
                 throw new ArgumentNullException(name, "Unexpected null value encountered.");
         }
 
+        [DebuggerHidden]
+        [DebuggerStepThrough]
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
-        public static void ValidArrayIndex(int value, Array array, string name)
+        public static void ValidArrayIndex(int value, Array array)
         {
-            Assert.NotNull(name);
-            NotNull(array, name);
+            NotNull(array);
 
             if (value < 0 || value >= array.Length)
-                throw new ArgumentOutOfRangeException(name, $"Expected greater than or equal to 0 and less than {array.Length}, found {value}.");
+                throw new EnsureException($"Expected greater than or equal to 0 and less than {array.Length}, found {value}.");
         }
 
+        [DebuggerHidden]
+        [DebuggerStepThrough]
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static void ValidArrayIndex(long value, Array array, string name)
         {
@@ -159,6 +334,20 @@ namespace XTypes
                 throw new ArgumentOutOfRangeException(name, $"Expected greater than or equal to 0 and less than {array.LongLength}, found {value}.");
         }
 
+
+        [DebuggerHidden]
+        [DebuggerStepThrough]
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static void ValidArrayIndex(int value, string array)
+        {
+            NotNull(array);
+
+            if (value < 0 || value >= array.Length)
+                throw new EnsureException($"Expected greater than or equal to 0 and less than {array.Length}, found {value}.");
+        }
+
+        [DebuggerHidden]
+        [DebuggerStepThrough]
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static void ValidArrayIndex(int value, string array, string name)
         {
@@ -169,6 +358,17 @@ namespace XTypes
                 throw new ArgumentOutOfRangeException(name, $"Expected greater than or equal to 0 and less than {array.Length}, found {value}.");
         }
 
+        [DebuggerHidden]
+        [DebuggerStepThrough]
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static void ValidString(string value)
+        {
+            if (String.IsNullOrWhiteSpace(value))
+                throw new EnsureException("Unexpected null or empty value encountered.");
+        }
+
+        [DebuggerHidden]
+        [DebuggerStepThrough]
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static void ValidString(string value, string name)
         {
@@ -178,6 +378,19 @@ namespace XTypes
                 throw new ArgumentNullException(name, "Unexpected null or empty value encountered.");
         }
 
+        [DebuggerHidden]
+        [DebuggerStepThrough]
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static void ValidUriString(string value)
+        {
+            ValidString(value);
+
+            if (!Uri.IsWellFormedUriString(value, UriKind.RelativeOrAbsolute))
+                throw new EnsureException("Unexpected malformed URI value encountered.", new UriFormatException($"'{value}' is invalid."));
+        }
+
+        [DebuggerHidden]
+        [DebuggerStepThrough]
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static void ValidUriString(string value, string name)
         {
@@ -189,6 +402,18 @@ namespace XTypes
                 throw new ArgumentException("Unexpected malformed URI value encountered.", name, new UriFormatException($"'{value}' is invalid."));
         }
 
+        [DebuggerHidden]
+        [DebuggerStepThrough]
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static void WithinRange<T>(T value, T lowerLimit, T upperLimit)
+            where T : IComparable, IComparable<T>
+        {
+            Greater(value, lowerLimit);
+            Less(value, upperLimit);
+        }
+
+        [DebuggerHidden]
+        [DebuggerStepThrough]
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static void WithinRange<T>(T value, T lowerLimit, T upperLimit, string name)
             where T : IComparable, IComparable<T>
@@ -198,5 +423,19 @@ namespace XTypes
             Greater(value, lowerLimit, name);
             Less(value, upperLimit, name);
         }
+    }
+
+    internal sealed class EnsureException : Exception
+    {
+        public EnsureException(string message)
+            : base(message)
+        { }
+
+        public EnsureException(string message, Exception innerException)
+            : base(message, innerException)
+        { }
+
+        internal EnsureException(System.Runtime.Serialization.SerializationInfo info, System.Runtime.Serialization.StreamingContext context)
+        { }
     }
 }
